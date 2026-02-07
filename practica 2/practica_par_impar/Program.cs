8602f991d88a1bce63af9ebc924a002003d776f9
Carlos <cd440034@gmail.com>
2026-02-07 03:14:22 -0400

    entrega de la practica #2

---
+++ practica 2/practica_par_impar/Program.cs
@@ -0,0 +1,25 @@
+﻿    int typeNumber = 0;
+        bool valido = false;
+
+        while (!valido)
+        {
+            try
+            {
+                Console.Write("Ingrese un numero entero: ");
+                typeNumber = Convert.ToInt32(Console.ReadLine()!);
+                valido = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Entrada de numero invalida:  favor de ingresarun numero entero");
+            }
+        }
+
+        if (typeNumber % 2 == 0)
+        {
+            Console.WriteLine($"El numero {typeNumber} es PAR");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {typeNumber} es IMPAR");
+        }
